ee28a447ce2828677e2eea6730eba3e243c3f625
Jason Allen Richard <Slayermonster0@gmail.com>
2019-09-30 21:37:11 -0700

    finished product

---
+++ models/Article.js
@@ -0,0 +1,35 @@
+
+var mongoose = require("mongoose");
+var Note = require("./Note");
+
+var Schema = mongoose.Schema;
+
+
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  notes: [{
+     type: Schema.Types.ObjectId,
+     ref: "Note"
+  }]
+});
+
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+
+module.exports = Article;
